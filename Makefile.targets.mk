54159894908c929a52ee883f917dfa9a74087941
Luke Shumaker <lukeshu@datawire.io>
2022-06-29 15:23:43 -0600

    Introduce tools.mk to manage required tools
    Signed-off-by: Luke Shumaker <lukeshu@datawire.io>

--- Makefile.targets.mk
+++ Makefile.targets.mk
@@ -48,6 +48,10 @@ help: ## Display this help
 
 ##@ Build
 
+.DEFAULT_GOAL = build
+
+include build-aux/tools.mk
+
 .PHONY: build build-linux-amd64 build-linux-arm64 build-all
 build: bin/${GOOS}/${GOARCH}/envoy-gateway ## Build the envoy-gateway binary
 build-linux-amd64: bin/linux/amd64/envoy-gateway
@@ -70,24 +74,32 @@ docker-push: ## Push the docker image for envoy-gateway.
 
 .PHONY: lint
 lint: ## Run lint checks
-lint: lint-golint lint-yamllint lint-codespell
+
+.PHONY: lint-deps
+lint-deps: ## Everything necessary to lint (useful to separate out in the logs)
 
 GOLANGCI_LINT_FLAGS ?= $(if $(GITHUB_ACTION),--out-format=github-actions)
 .PHONY: lint-golint
-lint-golint:
+lint: lint-golint
+lint-deps: $(tools/golangci-lint)
+lint-golint: $(tools/golangci-lint)
 	@echo Running Go linter ...
-	golangci-lint run $(GOLANGCI_LINT_FLAGS) --build-tags=e2e --config=tools/golangci-lint/.golangci.yml
+	$(tools/golangci-lint) run $(GOLANGCI_LINT_FLAGS) --build-tags=e2e --config=tools/golangci-lint/.golangci.yml
 
 .PHONY: lint-yamllint
-lint-yamllint:
+lint: lint-yamllint
+lint-deps: $(tools/yamllint)
+lint-yamllint: $(tools/yamllint)
 	@echo Running YAML linter ...
 	## TODO(lianghao208): add other directories later
-	yamllint --config-file=tools/yamllint/.yamllint changelogs/
+	$(tools/yamllint) --config-file=tools/yamllint/.yamllint changelogs/
 
 CODESPELL_FLAGS ?= $(if $(GITHUB_ACTION),--disable-colors)
 .PHONY: lint-codespell
+lint: lint-codespell
+lint-deps: $(tools/codespell)
 lint-codespell: CODESPELL_SKIP := $(shell cat tools/codespell/.codespell.skip | tr \\n ',')
-lint-codespell:
+lint-codespell: $(tools/codespell)
 # This ::add-matcher/::remove-matcher business is based on
 # https://github.com/codespell-project/actions-codespell/blob/2292753ad350451611cafcbabc3abe387491339a/entrypoint.sh
 # We do this here instead of just using
@@ -102,22 +114,22 @@ lint-codespell:
 	    printf '::add-matcher::$(CURDIR)/tools/codespell/matcher.json\n'; \
 	    trap "printf '::remove-matcher owner=codespell-matcher-default::\n::remove-matcher owner=codespell-matcher-specified::\n'" EXIT; \
 	  fi; \
-	  (set -x; codespell $(CODESPELL_FLAGS) --skip $(CODESPELL_SKIP) --ignore-words tools/codespell/.codespell.ignorewords --check-filenames --check-hidden -q2); \
+	  (set -x; $(tools/codespell) $(CODESPELL_FLAGS) --skip $(CODESPELL_SKIP) --ignore-words tools/codespell/.codespell.ignorewords --check-filenames --check-hidden -q2); \
 	}
 
 ##@ Development
 
 .PHONY: manifests
-manifests: controller-gen ## Generate WebhookConfiguration, ClusterRole and CustomResourceDefinition objects.
-	$(CONTROLLER_GEN) rbac:roleName=envoy-gateway-role crd webhook paths="./..." output:crd:artifacts:config=pkg/provider/kubernetes/config/crd/bases
+manifests: $(tools/controller-gen) ## Generate WebhookConfiguration, ClusterRole and CustomResourceDefinition objects.
+	$(tools/controller-gen) rbac:roleName=envoy-gateway-role crd webhook paths="./..." output:crd:artifacts:config=pkg/provider/kubernetes/config/crd/bases
 
 .PHONY: generate
-generate: controller-gen ## Generate code containing DeepCopy, DeepCopyInto, and DeepCopyObject method implementations.
-	$(CONTROLLER_GEN) object paths="./..."
+generate: $(tools/controller-gen) ## Generate code containing DeepCopy, DeepCopyInto, and DeepCopyObject method implementations.
+	$(tools/controller-gen) object paths="./..."
 
 .PHONY: kube-test
-kube-test: manifests generate lint envtest ## Run Kubernetes provider tests.
-	KUBEBUILDER_ASSETS="$(shell $(ENVTEST) use $(ENVTEST_K8S_VERSION) -p path)" go test ./... -coverprofile cover.out
+kube-test: manifests generate lint $(tools/setup-envtest) ## Run Kubernetes provider tests.
+	KUBEBUILDER_ASSETS="$(shell $(tools/setup-envtest) use $(ENVTEST_K8S_VERSION) -p path)" go test ./... -coverprofile cover.out
 
 ##@ Deployment
 
@@ -126,43 +138,11 @@ ifndef ignore-not-found
 endif
 
 .PHONY: kube-install
-kube-install: manifests kustomize ## Install CRDs into the Kubernetes cluster specified in ~/.kube/config.
-	$(KUSTOMIZE) build pkg/provider/kubernetes/config/crd | kubectl apply -f -
+kube-install: manifests $(tools/kustomize) ## Install CRDs into the Kubernetes cluster specified in ~/.kube/config.
+	$(tools/kustomize) build pkg/provider/kubernetes/config/crd | kubectl apply -f -
 
 .PHONY: kube-uninstall
-kube-uninstall: manifests kustomize ## Uninstall CRDs from the K8s cluster specified in ~/.kube/config. Call with ignore-not-found=true to ignore resource not found errors during deletion.
-	$(KUSTOMIZE) build pkg/provider/kubernetes/config/crd | kubectl delete --ignore-not-found=$(ignore-not-found) -f -
-
-##@ Build Dependencies
-
-## Location to install dependencies to
-LOCALBIN ?= $(shell pwd)/bin
-$(LOCALBIN):
-	mkdir -p $(LOCALBIN)
-
-## Tool Binaries
-KUSTOMIZE ?= $(LOCALBIN)/kustomize
-CONTROLLER_GEN ?= $(LOCALBIN)/controller-gen
-ENVTEST ?= $(LOCALBIN)/setup-envtest
-
-## Tool Versions
-KUSTOMIZE_VERSION ?= v3.8.7
-CONTROLLER_TOOLS_VERSION ?= v0.8.0
-
-KUSTOMIZE_INSTALL_SCRIPT ?= "https://raw.githubusercontent.com/kubernetes-sigs/kustomize/master/hack/install_kustomize.sh"
-.PHONY: kustomize
-kustomize: $(KUSTOMIZE) ## Download kustomize locally if necessary.
-$(KUSTOMIZE): $(LOCALBIN)
-	curl -s $(KUSTOMIZE_INSTALL_SCRIPT) | bash -s -- $(subst v,,$(KUSTOMIZE_VERSION)) $(LOCALBIN)
-
-.PHONY: controller-gen
-controller-gen: $(CONTROLLER_GEN) ## Download controller-gen locally if necessary.
-$(CONTROLLER_GEN): $(LOCALBIN)
-	GOBIN=$(LOCALBIN) go install sigs.k8s.io/controller-tools/cmd/controller-gen@$(CONTROLLER_TOOLS_VERSION)
-
-.PHONY: envtest
-envtest: $(ENVTEST) ## Download envtest-setup locally if necessary.
-$(ENVTEST): $(LOCALBIN)
-	GOBIN=$(LOCALBIN) go install sigs.k8s.io/controller-runtime/tools/setup-envtest@latest
+kube-uninstall: manifests $(tools/kustomize) ## Uninstall CRDs from the K8s cluster specified in ~/.kube/config. Call with ignore-not-found=true to ignore resource not found errors during deletion.
+	$(tools/kustomize) build pkg/provider/kubernetes/config/crd | kubectl delete --ignore-not-found=$(ignore-not-found) -f -
 
 .PHONY: FORCE
